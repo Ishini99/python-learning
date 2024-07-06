4cdd17012614f99f4e6e72bfb2b58a31dc85c3cd
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2024-07-07 00:39:26 +0530

    basic theory

---
+++ 1.py
@@ -0,0 +1,127 @@
+import datetime
+# print("hello")
+# print("It's a beautiful day in the neighborhood") 
+# #print('It's a beautiful day in the neighborhood') incorrect
+# print('It\'s a beautiful day in the neighborhood') 
+# print('Hickory Dickory Dock!\nThe mouse ran up the clock') 
+# print('''Hickory Dickory Dock!The mouse ran up the clock''')
+
+
+
+# print("There once was a movie star icon \n who preferred to sleep with the light on. \n They learned how to code \n a device that sure glowed \n and lit up the night using Python!")
+
+# #get inputs
+# name = input("What is your name?1  ")
+# print(name) 
+
+# Variable1 = input("What is your name? 2 ")
+# print(Variable1) 
+
+# # First Name = input("What is your name? ")
+# # print(First Name) cant keep space
+
+# Date = input("What is your name? 3" )
+# print(Date) 
+
+# # incorrect
+# # 3Name = input("What is your name? 4")
+# # print(3Name) 
+
+# DOB = input("What is your name? 5 ")
+# print(DOB) 
+
+# DateOfBirth = input("What is your name? 6 ")
+# print(DateOfBirth) 
+
+# YourFavoriteSignInTheHoroscope = input("What is your name? 7")
+# print(YourFavoriteSignInTheHoroscope) 
+
+# # combine variables and strings with the + symbol
+# firstName = input("What is your first name?")
+# lastName =input("What is your last name?")
+# print("Hello "+ " "+firstName+" " + lastName)
+
+# message='Hello world' 
+# print(message.lower()) 
+# print(message.upper()) 
+# print(message.swapcase())
+
+# message = 'Hello world' 
+# print(message.find('world')) 
+# print(message.count('o'))
+# print(message.capitalize()) 
+# print(message.replace('Hello','Hi'))
+
+# postalCode= " " #for good practice - initialize variablse
+# postalCode = input("Please enter your postal code:")
+# print(postalCode.upper())
+
+# print('I have %d cats'%2)
+# print('I have %2d cats'%2)
+# print('I have %3d cats'%2)
+# print('I have %4d cats'%2)
+# print('I have %02d cats'%2)
+# print('I have %03d cats'%2)
+# print('I have %04d cats'%2)
+# print('I have %f cats'%2)
+# print('I have %.2f cats'%2)
+
+
+
+# salary=input("Please enter your salary:")
+# bonus=input("Please enter your bonus: ")
+# payCheck=float(salary)+float(bonus)
+# print(payCheck)
+
+# today = datetime.date.today()
+# print (today)
+# print(today.year)
+# print(today.month)
+# print(today.day)
+
+# #strftime allows you to specify the date format 
+# print(today.strftime('%d %b,%Y'))
+# print(today.strftime('Please attend our event %A, %B %d in the year %Y'))
+
+# currentTime = datetime.datetime.now() 
+# print (currentTime) 
+# print (currentTime.hour) 
+# print (currentTime.minute)
+# print (currentTime.second)
+
+
+# answer=input("Would you like express shipping? ")
+# if answer =="yes":
+#     print("That will be an extra $10") 
+# print("Have a nice day")
+
+
+# deposit=input("How much would you like to deposit? ")
+
+# if int(deposit) >100:
+#    print("You get a free toaster!")
+# print("Have a nice day")
+
+
+# deposit=input("How much would you like to deposit? ")
+# if float(deposit) > 100 :
+#      print("You get a free toaster!")
+# else:
+# 	print("Enjoy your mug!")
+# print("Have a nice day")
+
+# if float(deposit) > 100 :
+#  #Set the boolean variable freeToaster to True
+#     freeToaster=True 
+# freeToaster=False 
+# print('hi')
+
+
+# #if the variable freeToaster is True  
+# #the print statement will execute 
+# if freeToaster :
+#     print("enjoy your toaster")
+# print("hello")
+
+for steps in range(20):
+    print('hi')
